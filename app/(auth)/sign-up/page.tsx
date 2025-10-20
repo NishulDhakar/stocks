e154b79c1dd78af1c305354d649cd34170ddbfd6
nishuldhakar <nishuldhakar.se@gmail.com>
2025-10-20 21:11:56 +0530

    updated auth logic

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,15 +4,13 @@ import {useForm} from "react-hook-form";
 import {Button} from "@/components/ui/button";
 
 import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
-
-// import FooterLink from "@/components/forms/FooterLink";
-// import {signUpWithEmail} from "@/lib/actions/auth.actions";
 import {useRouter} from "next/navigation";
-// import {toast} from "sonner";
+import {toast} from "sonner";
 import InputField from "@/components/Forms/inputField";
 import SelectField from "@/components/Forms/SelectField";
 import { CountrySelectField } from "@/components/Forms/CountrySelectField";
 import FooterLink from "@/components/Forms/FooterLink";
+import { signUpWithEmail } from "@/lib/actions/auth.actions";
 
 const SignUp = () => {
     const router = useRouter()
@@ -34,24 +32,24 @@ const SignUp = () => {
         mode: 'onBlur'
     }, );
 
-    // const onSubmit = async (data: SignUpFormData) => {
-    //     try {
-    //         const result = await signUpWithEmail(data);
-    //         if(result.success) router.push('/');
-    //     } catch (e) {
-    //         console.error(e);
-    //         toast.error('Sign up failed', {
-    //             description: e instanceof Error ? e.message : 'Failed to create an account.'
-    //         })
-    //     }
-    // }
+    const onSubmit = async (data: SignUpFormData) => {
+        try {
+            const result = await signUpWithEmail(data);
+            if(result.success) router.push('/');
+        } catch (e) {
+            console.error(e);
+            toast.error('Sign up failed', {
+                description: e instanceof Error ? e.message : 'Failed to create an account.'
+            })
+        }
+    }
 
     return (
         <>
             <h1 className="form-title">Sign Up & Personalize</h1>
 
-            <form  className="space-y-5">
-                    {/* <form onSubmit={handleSubmit(onSubmit)} className="space-y-5"></form> */}
+
+                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
                 <InputField
                     name="fullName"
                     label="Full Name"
---
+++ lib/nodemailer/index.ts
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer';
+import { NEWS_SUMMARY_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from './tamplates';
+
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.NODEMAILER_EMAIL!,
+        pass: process.env.NODEMAILER_PASSWORD!,
+    }
+})
+
+export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData) => {
+    const htmlTemplate = WELCOME_EMAIL_TEMPLATE
+        .replace('{{name}}', name)
+        .replace('{{intro}}', intro);
+
+    const mailOptions = {
+        from: `"Stocks" <nishuldhakar123@gmail.com>`,
+        to: email,
+        subject: `Welcome to Stocksapp - your stock market toolkit is ready!`,
+        text: 'Thanks for joining Stocksapp',
+        html: htmlTemplate,
+    }
+
+    await transporter.sendMail(mailOptions);
+}
+
+export const sendNewsSummaryEmail = async (
+    { email, date, newsContent }: { email: string; date: string; newsContent: string }
+): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', date)
+        .replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: `"Signalist News" <nishuldhakar123@gmail.com>`,
+        to: email,
+        subject: `📈 Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Signalist`,
+        html: htmlTemplate,
+    };
+
+    await transporter.sendMail(mailOptions);
+};
